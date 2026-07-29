169e65adb910ec79f9d13ca3bb3fae18a78d68f7
DesaleRohit <rohitdesale2005@gmail.com>
2026-07-29 16:54:18 +0530

    [ADDED]:Rohit - add project setup

---
+++ src/test/java/com/rohit/auth/RbacAuthSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacAuthSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
